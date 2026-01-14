3de945ba37e20fffd230a6fc15df6b9b247ac05b
ADRIAN <adriandeligero.1030@gmail.com>
2026-01-14 21:06:21 +0800

    cedula module: created add section

---
+++ treasurer/cedula/add.php
@@ -0,0 +1,10 @@
+<form method="POST" action="save.php">
+Full Name: <input name="full_name"><br>
+Address: <input name="address"><br>
+Age: <input name="age"><br>
+Birth Place: <input name="birth_place"><br>
+Occupation: <input name="occupation"><br>
+TIN: <input name="tin"><br>
+Amount: <input name="amount"><br>
+<button>Issue Cedula</button>
+</form>
