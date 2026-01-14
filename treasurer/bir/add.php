6beb7c996f2681f49b0c2a3a0b290644f288bd5a
ADRIAN <adriandeligero.1030@gmail.com>
2026-01-14 21:08:32 +0800

    bir module: created add section

---
+++ treasurer/bir/add.php
@@ -0,0 +1,7 @@
+<form method="POST" action="save.php">
+TIN: <input name="tin"><br>
+Payee: <input name="payee"><br>
+Gross Amount: <input name="gross"><br>
+Base Amount: <input name="base"><br>
+<button>Compute</button>
+</form>
