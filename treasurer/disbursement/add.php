e84457e4243f394a9d80e85f0d00cd5d22c0fef6
ADRIAN <adriandeligero.1030@gmail.com>
2026-01-14 21:09:48 +0800

    disbursement  module: created add section

---
+++ treasurer/disbursement/add.php
@@ -0,0 +1,11 @@
+<form method="POST" action="save.php">
+Date: <input type="date" name="date"><br>
+Check #: <input name="check_no"><br>
+Payee: <input name="payee"><br>
+DV No: <input name="dv_no"><br>
+Amount: <input name="amount"><br>
+Fund: <input name="fund"><br>
+Purpose: <input name="purpose"><br>
+Release Amount: <input name="release"><br>
+<button>Save</button>
+</form>
