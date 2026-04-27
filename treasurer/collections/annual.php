1137c1e2e5a38c3df5a8c9dfcaf9a21a8dcfd6f6
ADRIAN <adriandeligero.1030@gmail.com>
2026-04-28 00:44:35 +0800

    ready

--- treasurer/collections/annual.php
+++ treasurer/collections/annual.php
@@ -58,7 +58,7 @@ for ($month = 1; $month <= 12; $month++) {
 
 // Calculate yearly totals
 $yearlyCertificates = array_sum($monthlyData['certificates']);
-    $yearlyOtherCollections = array_sum($monthlyData['otherCollections']);
+$yearlyOtherCollections = array_sum($monthlyData['otherCollections']);
 $yearlyDocumentaryFees = array_sum($monthlyData['documentaryFees']);
 $yearlyGrandTotal = array_sum($monthlyData['totals']);
 
@@ -135,6 +135,10 @@ $monthNames = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July
             font-size: 16px;
         }
 
+        .total-row td {
+            color: #ffffff !important;
+        }
+
         .category-header {
             background: #f0f4f8 !important;
             color: #1e3a5f !important;
@@ -155,7 +159,22 @@ $monthNames = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July
             </div>
             <ul class="sidebar-menu">
                 <li><a href="../dashboard.php"><i class="fas fa-home"></i> Dashboard</a></li>
-                <li><details class="sidebar-dropdown"><summary><i class="fas fa-money-bill-wave"></i> Payments <i class="fas fa-chevron-right dropdown-caret"></i></summary><ul class="submenu"><li><a href="../payments/list.php"><i class="fas fa-list"></i> All Payments</a></li><li><a href="../payments/add.php"><i class="fas fa-plus"></i> Certificate</a></li><li><a href="../payments/manual.php?type=donation"><i class="fas fa-heart"></i> Donation</a></li><li><a href="../payments/manual.php?type=garbage"><i class="fas fa-trash"></i> Garbage</a></li><li><a href="../payments/manual.php?type=rental"><i class="fas fa-building"></i> Rental</a></li></ul></details></li>
+                <li>
+                    <details class="sidebar-dropdown">
+                        <summary><i class="fas fa-money-bill-wave"></i> Payments <i
+                                class="fas fa-chevron-right dropdown-caret"></i></summary>
+                        <ul class="submenu">
+                            <li><a href="../payments/list.php"><i class="fas fa-list"></i> All Payments</a></li>
+                            <li><a href="../payments/add.php"><i class="fas fa-plus"></i> Certificate</a></li>
+                            <li><a href="../payments/manual.php?type=donation"><i class="fas fa-heart"></i> Donation</a>
+                            </li>
+                            <li><a href="../payments/manual.php?type=garbage"><i class="fas fa-trash"></i> Garbage</a>
+                            </li>
+                            <li><a href="../payments/manual.php?type=rental"><i class="fas fa-building"></i> Rental</a>
+                            </li>
+                        </ul>
+                    </details>
+                </li>
                 <li><a href="../pending_payments/list.php"><i class="fas fa-hourglass-half"></i> Pending Status</a></li>
                 <li><a href="../cedula/list.php"><i class="fas fa-id-card"></i> Cedula</a></li>
                 <li><a href="../disbursement/list.php"><i class="fas fa-hand-holding-usd"></i> Disbursements</a></li>
@@ -339,8 +358,3 @@ $monthNames = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July
 </body>
 
 </html>
-
-
-
-
-
